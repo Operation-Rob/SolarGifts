04411faaa9de366ab1c2023e79c47240769644c9
Jack Sun <jsun1590@gmail.com>
2023-05-13 14:44:56 +0800

    changes

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,30 @@
-<script setup lang="ts">
-</script>
-
 <template>
-  <main>
-    <h1>hello</h1>  
-  </main>
+  <MapboxMap
+    :access-token="accessToken"
+    map-style="mapbox://styles/cjnbennett/clhli6yhp009p01rfbkox2bbg"
+    :center="westernAustralia"
+    :zoom="1"
+    class="w-full h-screen"
+  >
+    <div v-for="feature in dataset.features" :key="feature.id">
+      <CommunityMarker
+        :coordinates="feature.geometry.coordinates"
+        :image="feature.properties.image"
+        :description="feature.properties.description"
+      />
+    </div>
+  </MapboxMap>
 </template>
+
+<script setup lang="ts">
+import CommunityMarker from '@/components/Markers/CommunityMarker.vue'
+import { MapboxMap } from '@studiometa/vue-mapbox-gl'
+import dataset from '@/data/features.json'
+import 'mapbox-gl/dist/mapbox-gl.css'
+import { ref } from 'vue'
+
+const accessToken = ref(
+  'pk.eyJ1IjoiY2puYmVubmV0dCIsImEiOiJjbGhsaTRxc2EwOWw3M3FwOTQ0N3luaW5qIn0.8XbLwV61cr2oFs7ue0wCCw'
+)
+const westernAustralia = ref([121.8997, -25.5528])
+</script>
