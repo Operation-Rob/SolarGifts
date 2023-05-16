2059ebdc04b07b5d88ccaf6ca846e41b0d2e72d9
cjnbennett <conor.jn.bennett@gmail.com>
2023-05-16 18:13:18 +0800

    sadf

--- src/components/TopBar/TopBar.vue
+++ src/components/TopBar/TopBar.vue
@@ -1,6 +1,6 @@
 <template>
   <nav class="flex items-center p-2 justify-between">
-    <div>
+    <div class="flex items-center content-center">
       <img class="h-10" src="/images/logo.png" alt="" />
       <RouterLink to="/dashboard" class="mx-6 text-xl text-gray-900">Dashboard</RouterLink>
     </div>
